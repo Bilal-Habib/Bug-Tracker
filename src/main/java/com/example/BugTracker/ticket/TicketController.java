89ba4ad5100d7fa6b629c63bcb43d5f851bc38f2
Bilal Habib <bilal.habib67@gmail.com>
2021-08-04 16:59:56 +0100

    Changed project structure

--- src/main/java/com/example/BugTracker/ticket/TicketController.java
+++ src/main/java/com/example/BugTracker/ticket/TicketController.java
@@ -1,5 +1,6 @@
 package com.example.BugTracker.ticket;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class TicketController {
 
     private final TicketService ticketService;
 
+    @Autowired
     public TicketController(TicketService ticketService) {
         this.ticketService = ticketService;
     }
--- src/main/java/com/example/BugTracker/ticket/TicketService.java
+++ src/main/java/com/example/BugTracker/ticket/TicketService.java
@@ -1,7 +1,10 @@
 package com.example.BugTracker.ticket;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public class TicketService {
     public List<Ticket> getTickets() {
         return List.of(new Ticket(1, "Classes", "Create admin class", "Bug Tracker", "Low"));
