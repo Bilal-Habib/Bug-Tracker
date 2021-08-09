baa5ac3ce6c53c23123bfb62d9276b5ed7db7737
Bilal Habib <bilal.habib67@gmail.com>
2021-08-09 15:06:59 +0100

    Changed security config and login controller

--- src/main/java/com/example/BugTracker/login/SecurityConfig.java
+++ src/main/java/com/example/BugTracker/login/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.BugTracker.login;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -11,6 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/js/**", "/css/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
