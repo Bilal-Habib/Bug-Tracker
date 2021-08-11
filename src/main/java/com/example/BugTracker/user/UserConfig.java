bcadb481356d2fb6c4222951b24dc996aebaca0a
Chariton-Sapkas <csapkas@gmail.com>
2021-08-11 16:50:54 +0300

    fromyoutube

--- src/main/java/com/example/BugTracker/user/UserConfig.java
+++ src/main/java/com/example/BugTracker/user/UserConfig.java
@@ -8,22 +8,22 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
-@Configuration
-public class UserConfig {
+//@Configuration
+//public class UserConfig {
+//
+//    @Bean
+//    CommandLineRunner commandLineRunner(UserRepository repository) {
+//        return args -> {
+//            User Bilal = new User(
+//                    "Bilal",
+//                    "Bilal@gmail.com",
+//                    LocalDate.of(2001, Month.JULY, 7));
+//            User Chariton = new User(
+//                    "Chariton",
+//                    "Chariton@gmail.com",
+//                    LocalDate.of(1999, Month.AUGUST, 7));
+//            repository.saveAll(List.of(Bilal, Chariton));
+//        };
+//    }
 
-    @Bean
-    CommandLineRunner commandLineRunner(UserRepository repository) {
-        return args -> {
-            User Bilal = new User(
-                    "Bilal",
-                    "Bilal@gmail.com",
-                    LocalDate.of(2001, Month.JULY, 7));
-            User Chariton = new User(
-                    "Chariton",
-                    "Chariton@gmail.com",
-                    LocalDate.of(1999, Month.AUGUST, 8));
-            repository.saveAll(List.of(Bilal, Chariton));
-        };
-    }
-
-}
+//}
