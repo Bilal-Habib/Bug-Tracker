915f6cab33ffb2cd1dc78adcd658eb2073468250
Bilal Habib <bilal.habib67@gmail.com>
2021-08-11 17:44:01 +0100

    removed user files that are not needed

--- src/main/java/com/example/BugTracker/user/UserConfig.java
+++ src/main/java/com/example/BugTracker/user/UserConfig.java
@@ -10,20 +10,4 @@ import java.util.List;
 
 @Configuration
 public class UserConfig {
-
-    @Bean
-    CommandLineRunner commandLineRunner(UserRepository repository) {
-        return args -> {
-            User Bilal = new User(
-                    "Bilal",
-                    "Bilal@gmail.com",
-                    LocalDate.of(2001, Month.JULY, 7));
-            User Chariton = new User(
-                    "Chariton",
-                    "Chariton@gmail.com",
-                    LocalDate.of(1999, Month.AUGUST, 8));
-            repository.saveAll(List.of(Bilal, Chariton));
-        };
-    }
-
 }
